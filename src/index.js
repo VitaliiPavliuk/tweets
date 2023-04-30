ab77f3f1c070693fea0794f39d73fa625a0d47d4
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-04-30 20:12:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter
-        // basename="/tweets/"
-        >
+        <BrowserRouter basename="/tweets/">
           <App />
         </BrowserRouter>
       </PersistGate>
